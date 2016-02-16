993386f2b49025b695d2a95a201e185682a2dbc6
Robomandude <robomandood@gmail.com>
2016-02-15 23:32:42 -0600

    hacked together some more code

--- index.lua
+++ index.lua
@@ -1,19 +1,46 @@
 red = Color.new(255,0,0)
 blue = Color.new(0,0,255)
-green = Color.new(0,255,0)
+green = Color.new(0,200,0)
 white = Color.new(255,255,255)
 black = Color.new(0,0,0)
 dgray = Color.new(50,50,50)
 gray = Color.new(150,150,150)
 size = 0
-POC=""
+lines={{}}
 pad=Controls.read()
 tx,ty,ox,oy=0,0,0,0 --Touchscreen x and y and Previous frame tx and ty
 oldpad=pad --previous frame pad state
 
+curX=1 curY=1
+dispX=1 dispY=1
+function displayTop()
+
+	for y=1, #lines do
+	Screen.debugPrint(15,15,lines[y][x],blue,TOP_SCREEN)
+		for x=1 ,#lines[y] do
+			--Screen.debugPrint(x*15,y*20,lines[y][x],blue,TOP_SCEEN)
+		end
+	end
+end
+
+
+
+
 function Keyboard()
+
+
+
 	local keyset --pointer to beggining of character set for lowercase and UPPERCASE for entire keyboard
 	local ix,iy --integer x and y, lightly processed coordinate values for faster (cleaner?) code
+
+
+	if Controls.check(pad,KEY_DDOWN) then
+		curY=curY+1 end
+	if Controls.check(pad,KEY_DUP) then
+		curY=curY-1 end
+
+
+
 	
 	local keys = {"`1234567890-=",[[qwertyuiop[]\]],"asdfghjkl;'","zxcvbnm,./","~!@#$%^&*()_+",
 		"QWERTYUIOP{}|",[[ASDFGHJKL:"]],"ZXCVBNM<>?","TAB SPC","BCK DEL"}
@@ -48,7 +75,8 @@ function Keyboard()
 
 				if Controls.check(oldpad,KEY_TOUCH) and not Controls.check(pad,KEY_TOUCH) then 
 					tempC = green
-					POC = POC..inputChar
+					
+					table.insert(lines[curY],curX,inputChar) curX=curX+1 --the cursor moves when you type
 
 					else tempC=blue
 				 end 
@@ -68,14 +96,19 @@ function Keyboard()
 		inputChar=string.sub(keys[iy],ix,ix)
 		end
 	end
-	if inputChar then
-		Screen.debugPrint(6,40,inputChar,blue,TOP_SCREEN)
-	else
-		Screen.debugPrint(6,40,"nil",blue,TOP_SCREEN)
-	end
-	Screen.debugPrint(6,100,pad,blue,TOP_SCREEN)
-	Screen.debugPrint(6,80,POC,blue,TOP_SCREEN)
+	--if inputChar then
+	--	Screen.debugPrint(6,40,inputChar,blue,TOP_SCREEN)
+	--else
+	--	Screen.debugPrint(6,40,"nil",blue,TOP_SCREEN)
+	--end
+	--Screen.debugPrint(6,100,pad,blue,TOP_SCREEN)
+	--Screen.debugPrint(6,80,POC,blue,TOP_SCREEN)
 end
+
+
+
+
+
 
 
 
@@ -91,8 +124,10 @@ while true do
 	ox,oy=tx,ty
 	tx,ty=Controls.readTouch()
 	Screen.fillRect(5,300,5,20,green,TOP_SCREEN)
+	displayTop()
 	Keyboard()
-		Screen.debugPrint(6,6,tx,blue,TOP_SCREEN)
-		Screen.debugPrint(6,20,ty,blue,TOP_SCREEN)
+		--Screen.debugPrint(6,6,tx,blue,TOP_SCREEN)
+		--Screen.debugPrint(6,20,ty,blue,TOP_SCREEN)
 		Screen.flip()
+	if Controls.check(pad,KEY_B) then crash() end --faster restarting in citra
 end
