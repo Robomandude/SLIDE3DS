3664559563c554df915d17d94a9d5fb93d90fbe6
Robomandude <robomandood@gmail.com>
2016-02-29 16:27:58 -0600

    added space support and hacked in tab support!
    tab was tricky.  I used tab and empty strings to denote the 'length' of the tab.
    empty strings are handy because later when I use table.concat() it effectively ignores all the placeholder characters :D

--- index.lua
+++ index.lua
@@ -2,49 +2,73 @@ red = Color.new(255,0,0)
 blue = Color.new(0,0,255)
 green = Color.new(0,200,0)
 white = Color.new(230,230,230)
+wwhite = Color.new(255,255,255)
 black = Color.new(0,0,0)
 dgray = Color.new(50,50,50)
 gray = Color.new(150,150,150)
 size = 0
+TABSTOP=8
 lines={{}}
+SCREEN_WIDTH=30
 pad=Controls.read()
 tx,ty,ox,oy=0,0,0,0 --Touchscreen x and y and Previous frame tx and ty
 oldpad=pad --previous frame pad state
 
 cX=1 cY=1 sX=0 sY=0	--cursor xy and screen xy.  stores where the screen is in the file and where the cursor is on screen
-curX=1 curY=1		--where the cursor is in the file.  simply screen+cursor location
+
+function curX()		--I use screen position + cursor position so often I made functions for it
+	return cX+sX		--perhaps these functions are a good idea(?)
+end
+
+function curY()
+	return cY+sY
+end
+
 dispX=1 dispY=1
 
-function displayChar(x,y,char)
-	Screen.debugPrint(x*13+10,y*20-15,char or "",black,TOP_SCREEN)
-	end
+function typeTab() end
+function tabulate() end
 
+function getScrLoc(x,y)
+	return x*13,y*20-15
+end
 
 
 function displayTop()
 	Screen.fillRect(0,400,0,240,white,TOP_SCREEN)
-	Screen.fillRect(cX*13+10,cX*13+8,cY*20-15,cY*20,dgray,TOP_SCREEN)
+	if math.random()>.5 then
+	Screen.fillRect(cX*13,cX*13-2,cY*20-19,cY*20,dgray,TOP_SCREEN)
+	else
+	Screen.fillRect(cX*13,cX*13-2,cY*20-19,cY*20,gray,TOP_SCREEN)
+ end
 	for y=1, 12 do
 	
-	Screen.debugPrint(0,y*20-15,y+sY,black,TOP_SCREEN)
-			if lines[y+sY] then
-		for x=1 ,28 do
-			if lines[y+sY][x+sX] then
-				displayChar(x,y,lines[y+sY][x+sX])
-			end end
+		if lines[y+sY] then
+			for x=1 ,SCREEN_WIDTH do
+				if lines[y+sY][x+sX] then
+					if lines[y+sY][x+sX]=="" and lines[y+sY][x+sX+1]~="" then
+						Screen.fillRect(x*13+3,x*13+2,y*20-15,y*20,wwhite,TOP_SCREEN)
+					else
+						Screen.debugPrint(x*13,y*20-15,lines[y+sY][x+sX],black,TOP_SCREEN)
+						if lines[y+sY][x+sX]=="\t" then Screen.fillRect(x*13,x*13+2,y*20-
+
+15,y*20,green,TOP_SCREEN) end
+					end
+				end
+			end
 		end
 	end
 end
 
 function moveVertical()
-	if (cX+sX)>#lines[cY+sY] then
-		cX=#lines[cY+sY]-sX		--if the line you move to is shorter, moves cursor to end of line
-		if cX<1 then sX=#lines[cY+sY] cX=1 end	--moves screen to keep cursor on screen (if needed)
+	if (curX())>#lines[curY()] then
+		cX=#lines[curY()]-sX		--if the line you move to is shorter, moves cursor to end of line
+		if cX<1 then sX=#lines[curY()] cX=1 end	--moves screen to keep cursor on screen (if needed)
 	end
 end
 
 function moveUp()
-	if lines[cY+sY-1] then		--cant move cursor to before beggining of file
+	if lines[curY()-1] then		--cant move cursor to before beggining of file
 		if cY==1 then
 			sY = sY-1
 		else
@@ -58,15 +82,16 @@ function moveUp()
 end
 
 function moveDown()
-	if lines[cY+sY+1] then		--cant move cursor past end of file		
+	if lines[curY()+1] then		--cant move cursor past end of file		
 		if cY==11 then		--cant move cursor past edge of screen
 			sY = sY+1	--moves screen, keeps cursor still
 		else
 			cY = cY+1	--moves cursor, keeps screen still
 		end
-	moveVertical()
+		moveVertical()
 
-	return true end			--reports it successfully moved down/wasn't at end of the file
+		return true
+	end				--reports it successfully moved down/wasn't at end of the file
 
 end
 
@@ -74,50 +99,84 @@ function moveLeft()
 	if cX==1 then
 		if sX==0 then		--if its at the beggining of the line it moves up and to the end of the line
 			if moveUp() then
-				if #lines[cY+sY]>sX+28 then
-					cX=28
-					sX=#lines[cY+sY]-28
-				elseif #lines[cY+sY]<sX then
-					sX=#lines[cY+sY]
+				if #lines[curY()]>sX+SCREEN_WIDTH then
+					cX=SCREEN_WIDTH
+					sX=#lines[curY()]-SCREEN_WIDTH
+				elseif #lines[curY()]<sX then
+					sX=#lines[curY()]
 					cX=1
-				else cX=#lines[cY+sY]-sX+1
+				else cX=#lines[curY()]-sX+1
 				end
 			end
-		else sX=sX-1
+		else
+			sX=sX-1
 		end
 	else
-	cX=cX-1 end
+		cX=cX-1
+	end
+	
+
+
+while lines[curY()][curX()]=="" do
+	cX=cX-1
 end
+
+while cX<1 do
+	cX=cX+1
+	sX=sX-1
+end
+
+end
+
 function moveRight() --moves the cursor right
-	if lines[cY+sY][cX+sX] then 		--cant move cursor past end of line
-		if cX==28 then 			--cant move cursor past edge of screen
-			sX = sX+1 		--if end of screen move screen right and keep cursor still
-		else
-			cX = cX+1 		--otherwise move cursor right
+	if lines[curY()][curX()] then		--cant move cursor past end of line
+		cX=cX+1
+		while lines[curY()][curX()]=="" do
+			cX=cX+1
 		end
-	elseif moveDown() then sX=0 cX=1	--if at the end of the line tries to move down.  if successful sets x to beginning of line
+		while cX>= SCREEN_WIDTH do
+			cX=cX-1
+			sX=sX+1
+		end
+	elseif moveDown() then sX=0 cX=1	--if at the end of the line tries to move down.  if successful sets x to beginning of 
+
+line
 	else return
 	end
 	return true
 end
-			
+
+function insertChar(y,x,t)				--I use this function because specifying the end of a table does not work 
+						--	well, I have to not specify anything instead
+	if #lines[y]==x then
+		table.insert(lines[y],t)
+	else
+		table.insert(lines[y],x,t)
+	end
+end
 
 function typeChar(char)
+
+	if char=="\t" then
+		return typeTab
+	end
+
 	return function() 
-		if #lines[sY+cY]==cX+sX then
-		table.insert(lines[sY+cY],char)
-		else
-		table.insert(lines[sY+cY],cX+sX,char)
-		end
+		insertChar(curY(),curX(),char)
+		tabulate(curY(),curX())
 		moveRight()
 	end
 end
 
 function backspace()
-	if cX+sX>1 then
-		table.remove(lines[sY+cY],cX+sX-1) moveLeft()
-	elseif cX+sX==1 and cY+sY~=1 then
-		local _=sY+cY
+	if curX()>1 then
+		moveLeft()
+		
+		repeat
+			table.remove(lines[curY()],curX())
+		until lines[curY()][curX()]~=""
+	elseif curX()==1 and curY()~=1 then
+		local _=curY()
 		moveLeft()
 		for i=1,#lines[_] do
 			table.insert(lines[_-1],lines[_][i])
@@ -125,17 +184,48 @@ function backspace()
 		table.remove(lines,_)
 	end
 end
+
 function typeDelete()
 	if moveRight() then backspace() end 	--delete is just backspace after pressing right -that is, if pressing right did anything
 end
 
+function tabulate(y)
+	local x=1
+	while lines[y][x] do
+		if lines[y][x]=="\t" then
+			x=x+1
+			while x%TABSTOP~=1 do
+				if lines[y][x]~="" then
+					insertChar(y,x,"")
+				end
+			x=x+1
+			end
 
+			while lines[y][x]=="" do
+				table.remove(lines[y],x)
+			end
+		else
+			x=x+1
+		end
+	end
+end
+
+			
+
+
+
+
+function typeTab()
+	insertChar(curY(),curX(),"\t")
+	tabulate(curY(),curX())
+	moveRight()
+end
 
 
 function typeEnter()
-	table.insert(lines,sY+cY+1,{})
-	while lines[sY+cY][sX+cX] do
-		table.insert(lines[sY+cY+1],table.remove(lines[sY+cY],sX+cX))
+	table.insert(lines,curY()+1,{})
+	while lines[curY()][curX()] do
+		table.insert(lines[curY()+1],table.remove(lines[curY()],curX()))
 	end
 	moveDown()
 	sX=0 cX=1
@@ -152,8 +242,8 @@ function Keyboard()
 	local ix,iy --integer x and y, lightly processed coordinate values for faster (cleaner?) code
 
 	
-	local keys = {"`1234567890-=",[[qwertyuiop[]\]],"asdfghjkl;'","zxcvbnm,./","~!@#$%^&*()_+",
-		"QWERTYUIOP{}|",[[ASDFGHJKL:"]],"ZXCVBNM<>?","TAB SPC","BCK DEL"}
+	local keys = {"`1234567890-=",[[qwertyuiop[]\]],"asdfghjkl;'","zxcvbnm,./  \t","~!@#$%^&*()_+",
+		"QWERTYUIOP{}|",[[ASDFGHJKL:"]],"ZXCVBNM<>?  \t","TAB SPC","BCK DEL"}
 
 	if Controls.check(pad,KEY_A) then
 
@@ -207,7 +297,9 @@ function Keyboard()
 				if Controls.check(oldpad,KEY_TOUCH) and not Controls.check(pad,KEY_TOUCH) then 
 					tempC = green
 					
+
 					typeChar(inputChar)()
+
 
 					else tempC=blue
 				 end 
@@ -227,10 +319,11 @@ function Keyboard()
 		inputChar=string.sub(keys[iy],ix,ix)
 		end
 	end
-	Screen.debugPrint(5,150,cX,white,BOTTOM_SCREEN)
+	--Screen.debugPrint(5,150,cX,white,BOTTOM_SCREEN)
 	Screen.debugPrint(5,170,cY,white,BOTTOM_SCREEN)
 	Screen.debugPrint(5,200,sX,white,BOTTOM_SCREEN)
 	Screen.debugPrint(5,220,sY,white,BOTTOM_SCREEN)
+	Screen.debugPrint(5,150,string.format("Line: %i",curY()),white,BOTTOM_SCREEN)
 	--if inputChar then
 	--	Screen.debugPrint(6,40,inputChar,blue,TOP_SCREEN)
 	--else
